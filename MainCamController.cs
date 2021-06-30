e9f09452ab3e6e003f0e9458f223c350a93f4b03
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-06-30 11:24:21 -0700

    Added NPC controller scripts and tried to implement a camera system. FIX ME: FIX THE CAMERA CONTROL SYSTEM!

--- MainCamController.cs
+++ MainCamController.cs
@@ -5,16 +5,52 @@ using UnityEngine;
 public class MainCamController : MonoBehaviour
 {
     public GameObject player;
+    public GameObject firstCam;
     public GameObject secondCam;
-    private Vector3 offset = new Vector3(0, 7, -12);
+    public GameObject thirdCam;
+    public GameObject [] camVec;
+    private int currCam;
+    private int numCams;
+    private Vector3 camOffFirst;
+    private Vector3 camOffSecond;
+    private Vector3 camOffThird;
+    private Vector3[] camOffVec;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
+        firstCam.gameObject.SetActive(true);
         secondCam.gameObject.SetActive(false);
-        this.gameObject.SetActive(true);
+        thirdCam.gameObject.SetActive(false);
+        
+        numCams = 3;
+        camVec[0] = firstCam;
+        camVec[1] = secondCam;
+        camVec[2] = thirdCam;
+
+        currCam = 0;
+        
+        camOffFirst = new Vector3(0, 7, -12);
+        camOffSecond = new Vector3(-0.3f, 0.86f, -7.9f);
+        camOffThird = new Vector3(-3, 1.95f, 10.5f);
+        offset = camOffFirst;
+        camOffVec[0] = camOffFirst;
+        camOffVec[1] = camOffSecond;
+        camOffVec[2] = camOffThird;
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            camVec[currCam].gameObject.SetActive(false);
+            currCam = (currCam + 1) % numCams;
+            camVec[currCam].gameObject.SetActive(true);
+            offset = camOffVec[currCam];
+        }
+    }
+
+    // LateUpdate is called once per frame after the Update method
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
