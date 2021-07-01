350b6629b9aaba41e6c9bac652715ff851822210
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-07-01 09:21:54 -0700

    Fixed the camera switching mechanism. I would've liked for them to be in an array but oh well. All known bugs resolved (so far...)

--- CameraSystemController.cs
+++ CameraSystemController.cs
@@ -7,27 +7,30 @@ public class CameraSystemController : MonoBehaviour
     public GameObject firstCam;
     public GameObject secondCam;
     public GameObject thirdCam;
-    public GameObject[] camVec;
+    //public GameObject[] camVec;               //Tried to implement using an array but ran into issues. Implemented it using a simpler process instead.
     private int currCam;
     private int numCams;
 
     // Start is called before the first frame update
     void Start()
     {
-        firstCam.gameObject.SetActive(false);
+        firstCam.gameObject.SetActive(true);
         secondCam.gameObject.SetActive(false);
-        thirdCam.gameObject.SetActive(true);
+        thirdCam.gameObject.SetActive(false);
 
-        Debug.Log(camVec.Length);
-
+        //Debug.Log(camVec.Length);
         numCams = 3;
+        currCam = 0;
+        /*
+        
         camVec[0] = firstCam;
         camVec[1] = secondCam;
         camVec[2] = thirdCam;
 
-        currCam = 0;
+        
 
         Debug.Log(camVec.Length);
+        */
     }
 
     // Update is called once per frame
@@ -36,11 +39,34 @@ public class CameraSystemController : MonoBehaviour
         if (Input.GetKeyDown("c"))
         {
             Debug.Log("C Pressed!");
-            camVec[currCam].gameObject.SetActive(false);
-            currCam = (currCam + 1) % numCams;
-            camVec[currCam].gameObject.SetActive(true);
+            currCam++;
+            currCam %= numCams;
+            //camVec[currCam].gameObject.SetActive(false);
+            //currCam = (currCam + 1) % numCams;
+            //camVec[currCam].gameObject.SetActive(true);
+            switch (currCam)
+            {
+                case 0:
+                    firstCam.gameObject.SetActive(true);
+                    thirdCam.gameObject.SetActive(false);
+                    break;
+
+                case 1:
+                    secondCam.gameObject.SetActive(true);
+                    firstCam.gameObject.SetActive(false);
+                    break;
+
+                case 2:
+                    thirdCam.gameObject.SetActive(true);
+                    secondCam.gameObject.SetActive(false);
+                    break;
+
+                default:
+                    Debug.Log("ERROR: CAMERA SWITCH MECHANISM HAS A PROBLEM!");
+                    break;
+            }
             Debug.Log("Cam Activation Process Complete!");
-            Debug.Log(camVec.Length);
+            //Debug.Log(camVec.Length);
         }
     }
 }
