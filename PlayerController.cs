1ce8229c752d3bb28f6dc271ad06e21263f8c66d
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-07-02 09:08:20 -0700

    Added wheel rotation when the vehicle is moving forward. Tried to implement wheel turning animations but ended up with hilarious results so disabled that... for now...

--- PlayerController.cs
+++ PlayerController.cs
@@ -12,6 +12,13 @@ public class PlayerController : MonoBehaviour
     //public const float gravityForce = 9.8f;
     public float horizontalInput;
     public float forwardInput;
+
+    public GameObject wheel1;           // wheel front left
+    public GameObject wheel2;           // wheel front right
+    public GameObject wheel3;           // wheel back left
+    public GameObject wheel4;           // wheel back right
+
+
     //Rigidbody rb;
     //private float mass;
 
@@ -41,7 +48,42 @@ public class PlayerController : MonoBehaviour
         if (forwardInput != 0)
         {
             transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+            if (forwardInput > 0)
+            {
+                wheelRotate(Vector3.right);
+            }
+            else
+            {
+                wheelRotate(Vector3.left);
+            }
+            /*
+            if (horizontalInput > 0)
+            {
+                wheelTurn(Vector3.up);
+            }
+            else
+            {
+                wheelTurn(Vector3.down);
+            }
+            */
         }
         
+    }
+
+    void wheelRotate(Vector3 direction)
+    {
+        float wheelSpeed = 50.0f;
+        wheel1.transform.Rotate(direction, forwardInput * Time.deltaTime * velocity * wheelSpeed);
+        wheel2.transform.Rotate(direction, forwardInput * Time.deltaTime * velocity * wheelSpeed);
+        wheel3.transform.Rotate(direction, forwardInput * Time.deltaTime * velocity * wheelSpeed);
+        wheel4.transform.Rotate(direction, forwardInput * Time.deltaTime * velocity * wheelSpeed);
+
+    }
+    void wheelTurn (Vector3 direction)
+    {
+        float wheelSpeed = 50.0f;
+        wheel1.transform.Rotate(direction, forwardInput * Time.deltaTime * velocity * wheelSpeed);
+        wheel2.transform.Rotate(direction, forwardInput * Time.deltaTime * velocity * wheelSpeed);
+
     }
 }
